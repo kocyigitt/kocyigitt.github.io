efa59d82361c3a44d5e226d5cb5ce9e428b3e909
İsa Koçyiğit <151690216+kocyigitt@users.noreply.github.com>
2023-11-22 02:35:24 +0300

    Add files via upload

---
+++ cdn/script.js
@@ -0,0 +1,7 @@
+$.ajax({
+  url: "https://spotify.aidenwallis.co.uk/u/655d3beae1ec4a6adbdd2910",
+  method: "GET"
+}).then(function(data) {
+  adminlisten = data.replace('User is currently not playing any music on Spotify.', 'currently not playing any music on Spotify.');
+  $("#track").text(adminlisten);
+});
